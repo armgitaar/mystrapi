81d780d5edf9ebe2fa0cf9b76759e91249e4734b
Adam Miedema <armgitaar@yahoo.com>
2020-09-15 14:24:45 -0600

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbeef6fcb52681c336995ab32a6764f7'),
+    },
+  },
+});
